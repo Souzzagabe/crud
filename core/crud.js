ef322899134b457e0730c127f83e1625883e77b4
souzzagab <gabriel.souza@lacreisaude.com.br>
2024-10-01 15:36:41 -0300

    first commit

---
+++ core/crud.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const DB_FILE_PATH = "./core/db"
+
+
+console.log("[CRUD]");
+
+function create(content) {
+
+    fs.writeFileSync(DB_FILE_PATH, content)
+    return content;
+}
+
+// SIMULATION
+
+create("Hoje eu preciso gravar aulas!")
