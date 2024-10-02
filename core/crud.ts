1ab00e75ada011c85217c5a39f268cc09e0b801b
souzzagab <gabriel.souza@lacreisaude.com.br>
2024-10-01 23:23:35 -0300

    feature: delete function

--- core/crud.ts
+++ core/crud.ts
@@ -1,5 +1,5 @@
 // const fs = require('fs') - commonJS 
-
+import { v4 as uuid } from 'uuid';
 import fs from "fs"
 
 const DB_FILE_PATH = "./core/db"
@@ -8,14 +8,16 @@ const DB_FILE_PATH = "./core/db"
 console.log("[CRUD]");
 
 interface Todo {
+    id: string;
     date: string;
     content: string;
     done: boolean;
 }
 
 
-function create(content: string) {
+function create(content: string): Todo {
     const todo: Todo = {
+        id: uuid(),
         date: new Date().toISOString(),
         content: content,
         done: false,
@@ -30,7 +32,7 @@ function create(content: string) {
         todos,
     }, null, 2))
 
-    return content;
+    return todo;
 }
 
 function read(): Array<Todo>  {
@@ -42,13 +44,46 @@ function read(): Array<Todo>  {
     return db.todos;
 }
 
+function update(id: string, partialTodo: Partial<Todo>) {
+    let updatedTodo;
+    const todos = read();
+    todos.forEach((currentTodo) =>{
+        const isToUpdate = currentTodo.id === id;
+        if(isToUpdate) {
+           updatedTodo = Object.assign(currentTodo, partialTodo)
+        }
+    })
+
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+        todos,
+    }, null, 2));
+    if(!updatedTodo) {
+        throw new Error(`Todo with id ${id} not found`)
+    }
+    return updatedTodo;
+}
+
+function updatedContentById(id: string, content: string): Todo {
+    return update(id, {
+     content,
+    })
+}
+ 
 function CLEAR_DB() {
     fs.writeFileSync(DB_FILE_PATH, "")
 }
 
 
 // SIMULATION
+CLEAR_DB()
+create("priemira todo")
+create("priemira todo")
+const terceiraTodo = create("segunda TODO")
+// update(terceiraTodo.id, {
+//     content: "Atualizada",
+//     done: true,
+// });
 
-create("segunda todo")
+updatedContentById(terceiraTodo.id, "Atualizada")
 
 console.log(read())
